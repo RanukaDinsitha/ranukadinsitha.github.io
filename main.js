8e90e3611e3013a159b5f5edd56f402e8effe1e3
RanukaDinsitha <140217957+RanukaDinsitha@users.noreply.github.com>
2023-08-01 20:23:34 +1200

    HUGE UPDATE!!!

---
+++ main.js
@@ -0,0 +1,5 @@
+window.location.href = "old.html";
+console.log("Welcome To The Dev Console!");
+console.log("If you are not a dev please close this NOW.");
+console.log("It can change what will happen to the the site...");
+console.log("So Please close me!");
--- script.js
+++
@@ -1,2 +0,0 @@
-console.log("Welcome to The Dev Console!")
-console.log("Please Don't paste Anything in here!")
