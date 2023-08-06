2e824620b6870463af4a3082aa55eedf05753d30
RanukaDinsitha <140217957+RanukaDinsitha@users.noreply.github.com>
2023-08-06 02:15:21 +0000

    Blah

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.location.href = "old.html";
+window.location.href = "red.html";
 console.log("Welcome To The Dev Console!");
 console.log("If you are not a dev please close this NOW.");
 console.log("It can change what will happen to the the site...");
